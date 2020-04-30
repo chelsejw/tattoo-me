6019e7071c1e79be05d929dfe3fa58e7d6b5aaf4
chelsejw <chelsejw@gmail.com>
2020-04-30 20:17:24 +0800

    add artist img urls as part of results for queries

--- models/artists.js
+++ models/artists.js
@@ -7,7 +7,8 @@ module.exports = (dbPoolInstance) => {
     // `dbPoolInstance` is accessible within this function scope
 
     const getAll = (callback) => {
-        let query = "SELECT artist_id, artist_username, artist_displayname, artists.location_id, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id ORDER BY artist_id ASC";
+        let query =
+          "SELECT artist_id, artist_username, artists.artist_img, artist_displayname, artists.location_id, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id ORDER BY artist_id ASC";
 
         dbPoolInstance.query(query, (err, result) => {
             callback(err, result.rows);
@@ -38,8 +39,7 @@ module.exports = (dbPoolInstance) => {
 
     const getArtistsByLocation = (locationId, callback) => {
 
-        let query =
-            `SELECT artists.artist_id, artist_username, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists.location_id = ${locationId} ORDER BY artist_id ASC`;
+        let query = `SELECT artists.artist_id, artist_username, artists.artist_img, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists.location_id = ${locationId} ORDER BY artist_id ASC`;
 
         dbPoolInstance.query(query, (err, result) => {
             //If more than one item is returned
@@ -55,7 +55,7 @@ module.exports = (dbPoolInstance) => {
     }
 
     const getArtistsByHashtag = (hashtagId, callback) => {
-        let query = `SELECT artists.artist_id, artist_username, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists_hashtags.hashtag_id = ${hashtagId} ORDER BY artist_id ASC`;
+        let query = `SELECT artists.artist_id, artists.artist_img, artist_username, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists_hashtags.hashtag_id = ${hashtagId} ORDER BY artist_id ASC`;
         dbPoolInstance.query(query, (err, result) => {
             //If more than one item is returned
             if (result.rows.length > 1) {
@@ -70,7 +70,7 @@ module.exports = (dbPoolInstance) => {
     };
 
     const getArtistsByHashtagAndLocation = (hashtagId, locationId, callback) => {
-        let query = `SELECT artists.artist_id, artist_username, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists_hashtags.hashtag_id = ${hashtagId} AND artists.location_id = ${locationId} ORDER BY artist_id ASC`;
+        let query = `SELECT artists.artist_id, artists.artist_img, artist_username, artist_displayname, artists.location_id, hashtag_name, location_name, email, booking_avail FROM artists INNER JOIN locations ON locations.location_id = artists.location_id INNER JOIN artists_hashtags ON artists.artist_id = artists_hashtags.artist_id INNER JOIN hashtags ON hashtags.hashtag_id = artists_hashtags.hashtag_id WHERE artists_hashtags.hashtag_id = ${hashtagId} AND artists.location_id = ${locationId} ORDER BY artist_id ASC`;
 
         dbPoolInstance.query(query, (err, result) => {
 
